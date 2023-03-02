81a67550a9e6b7d79f910f3b11627a551e17160c
Bogdan Radchenko <inroph@mail.ru>
2023-03-02 23:55:26 +0300

    adding logic for creating persons and screens in main storyboard

--- ContactList/Models/Person.swift
+++ ContactList/Models/Person.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct Person {
+    let fullName: String
+    let number: String
+    let emailAddress: String
+    
+    static func getPerson() -> [Person] {
+        
+        var persons: [Person] = []
+        
+        var shuffledNames = names.shuffled()
+        var shuffledSurnames = surnames.shuffled()
+        var shuffledNumbers = phoneNumbers.shuffled()
+        var shuffledEmails = emails.shuffled()
+        
+        while shuffledNames.count != 0 {
+            let person = Person(fullName: shuffledNames.remove(at: Int.random(in: 0..<shuffledNames.count)) + " " + shuffledSurnames.remove(at: Int.random(in: 0..<shuffledSurnames.count)), number: shuffledNumbers.remove(at: Int.random(in: 0..<shuffledNumbers.count)), emailAddress: shuffledEmails.remove(at: Int.random(in: 0..<shuffledEmails.count)))
+            
+            persons.append(person)
+        }
+        
+        return persons
+    }
+}
+
+
--- ContactList/Services/DataStore.swift
+++ ContactList/Services/DataStore.swift
@@ -4,3 +4,11 @@
 //
 //  Created by Богдан Радченко on 02.03.2023.
 //
+
+let names = ["Alex", "Ben", "Din", "Mike"]
+
+let surnames = ["Smith", "Johnson", "Jackson", "White"]
+
+let phoneNumbers = ["111-111-111", "222-222-222", "333-333-333", "444-444-444"]
+
+let emails = ["home@gmail.con", "work@gmail.com", "user1@gmail.com", "price@gmail.com"]
