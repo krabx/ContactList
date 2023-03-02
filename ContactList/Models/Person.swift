5896f12aa74580eae3d596759a8924e68cb38f4a
Bogdan Radchenko <inroph@mail.ru>
2023-03-02 18:54:08 +0300

    Grouping files by groups

---
+++ ContactList/Models/Person.swift
@@ -0,0 +1,8 @@
+//
+//  Person.swift
+//  ContactList
+//
+//  Created by Богдан Радченко on 02.03.2023.
+//
+
+import Foundation
---
+++ ContactList/Services/DataStore.swift
@@ -0,0 +1,6 @@
+//
+//  DataStore.swift
+//  ContactList
+//
+//  Created by Богдан Радченко on 02.03.2023.
+//
