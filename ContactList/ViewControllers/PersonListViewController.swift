9ffdaf3407d692ba9b58d2b89c708ea9a826f02f
Bogdan Radchenko <inroph@mail.ru>
2023-03-08 15:26:31 +0300

    adding deselected to cell

--- ContactList/ViewControllers/PersonListViewController.swift
+++ ContactList/ViewControllers/PersonListViewController.swift
@@ -42,4 +42,8 @@ extension PersonListViewController {
 
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
